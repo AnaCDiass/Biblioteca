74a5d2f551df94c93240456e10e33b1bb5c8f664
AnaCDiass <anaclaradiasq@gmail.com>
2025-05-06 07:43:00 -0300

    Add files via upload

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,7 @@
-from django.views import view
-from django.shortcuts import FormHelper, Layout, Submit
+#from django.views import TemplateView
+#from django.shortcuts import FormHelper, Layout, Submit
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
 from django.contrib import messages
 from .models import Livro
 
@@ -8,7 +10,8 @@ from django import forms
 class LivroForm(forms.ModelForm):
     class Meta:
         model = Livro
-        fields = '__all__'
+        #fields = '__all__'
+        fields = ['nome', 'autor', 'editora', 'genero', 'preco', 'data_plub', 'status']
 
     def __init__(self, *args, **kwargs):
         super(LivroForm, self).__init__(*args, **kwargs)
